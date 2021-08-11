99fe5fdf0c9227af5e4c4c69dad99ce791e2aa55
Joonkyo Kim <rootwarp@gmail.com>
2021-08-11 15:41:46 +0900

    CPU / Mem metrics

--- exporter/system_exporter_test.go
+++ exporter/system_exporter_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -16,13 +17,10 @@ import (
 
 var (
 	errMetricNotFound = errors.New("Specified metric cannot be found")
+	testExporter      = createSystemExporter().(*systemExporter)
 )
 
 func TestCPU(t *testing.T) {
-	testExporter := createSystemExporter().(*systemExporter)
-
-	assert.NotNil(t, testExporter)
-
 	// Set dummy
 	testExporter.cpuUsage.Set(10.1234)
 	testExporter.memoryUsage.Set(23.12345)
@@ -47,6 +45,25 @@ func TestCPU(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "10.1234", cpu)
 	assert.Equal(t, "23.12345", memory)
+}
+
+func TestGetSystemMetric(t *testing.T) {
+	load, idle, err := testExporter.getCPUUsage(1 * time.Second)
+
+	assert.Nil(t, err)
+	assert.InDelta(t, 100, load+idle, 0.1)
+
+	used, free, err := testExporter.getMemUsage()
+
+	assert.Nil(t, err)
+	assert.InDelta(t, 100, used+free, 0.1)
+}
+
+func TestDiskMetric(t *testing.T) {
+	free, total, err := testExporter.getDiskUsage("/")
+
+	assert.Nil(t, err)
+	assert.True(t, free < total)
 }
 
 func findMetric(name string, content io.Reader) (string, error) {
